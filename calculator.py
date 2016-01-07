9ba3f4e2b578b6394a56bf20a10c6746d8b23e33
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 17:40:48 -0800

    removed unnecessary comments

--- calculator.py
+++ calculator.py
@@ -48,8 +48,7 @@ def calculate(tokens):
     else: 
         print "I don't understand!"
 
-# Your code goes here
-# No setup
+
 while True:
     calc_input = raw_input("> ")
 
@@ -57,10 +56,7 @@ while True:
     
     if calc_tokens[0] == 'q':
         break
-#     otherwise decide which math function to call based on the tokens we read
     else:
         calculate(calc_tokens)
 
-# tokenize("+ 0 1")
-# tokenize("mod 10 3")
-# calculate(["+", "2", "6"])
+
